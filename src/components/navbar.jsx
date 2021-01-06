f85a83dc2b7f6ddb0849de682428000243fabb4b
abhisheksaxena1998 <abhi123923@gmail.com>
2021-01-06 13:28:06 +0530

    changes favicon

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -5,7 +5,11 @@ const NavBar = (props) => {
     <nav className="navbar navbar-light bg-light">
       <div className="container-fluid">
         <a className="navbar-brand">
-          <img src="https://www.any.do/v4/images/og-image.png" height="50" />
+          <img
+            src="https://raw.githubusercontent.com/abhisheksaxena1998/reactToDoApplication/main/src/og-image.png"
+            height="60"
+            loading="lazy"
+          />
           <strong>
             {"  "}
             To-Do Application{" "}
