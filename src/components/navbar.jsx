ce1a15eeb4f5cb6e6308598f886dfea2222c9689
abhisheksaxena1998 <abhi123923@gmail.com>
2021-01-06 13:18:58 +0530

    added swal alerts

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -5,12 +5,7 @@ const NavBar = (props) => {
     <nav className="navbar navbar-light bg-light">
       <div className="container-fluid">
         <a className="navbar-brand">
-          <img
-            src="https://www.any.do/v4/images/og-image.png"
-            height="50"
-            alt=""
-            loading="lazy"
-          />
+          <img src="https://www.any.do/v4/images/og-image.png" height="50" />
           <strong>
             {"  "}
             To-Do Application{" "}
